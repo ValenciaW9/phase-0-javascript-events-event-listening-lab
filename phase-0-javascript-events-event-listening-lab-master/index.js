b1e0bf385e9d5f6b25713de378a8438fcace8673
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-15 16:27:24 -0400

    phase-0-javascript-events-event-listening-lab/
    phase-0-javascript-events-event-listening-lab/

--- phase-0-javascript-events-event-listening-lab-master/index.js
+++ phase-0-javascript-events-event-listening-lab-master/index.js
@@ -1,12 +1,16 @@
 function addingEventListener() {
-    // Get the button element from the HTML document
-    const button = document.querySelector('#myButton');
-  
-    // Add event listener to the button
-    button.addEventListener('click', function() {
-      // Code to be executed when the button is clicked
-      console.log('The button was clicked!');
-    });
-  
-    console.log('The code is running!');
-  }
+  // Get the button element from the HTML document
+  const button = document.querySelector('#myButton');
+
+  // Add event listener to the button
+  button.addEventListener('click', function() {
+    // Code to be executed when the button is clicked
+    console.log('The button was clicked!');
+  });
+
+  console.log('The code is running!');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  addingEventListener();
+});
